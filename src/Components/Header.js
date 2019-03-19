0eabdcfdad7d1ce4c6d4a906b694adeefd137b7f
samwijayag <sam.wijayag@gmail.com>
2019-03-18 18:28:07 -0700

    app working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Header = () => {
-	return(){
-		<div></div>
-	}
+	return(
+		<header> 
+			<img 
+                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                alt="Trollface"
+            />
+            <h1>Meme Generator</h1>
+		</header>
+	)
 }
 
 export default Header;
